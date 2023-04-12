4fa26e5540051440abf70acdd853d413a9ea3970
zhangxueliang <zhangxueliang@dameng.com>
2023-04-12 13:46:52 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -6,3 +6,9 @@ go mod tidy
 
 kubebuilder create api --group batch --version v1 --kind Mysql # y y
 
+make install && make run 
+
+kubectl apply -f config/samples/batch_v1_mysql.yaml
+
+kubectl scale mysql mysql-sample --replicas 2
+
